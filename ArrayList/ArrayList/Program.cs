035b3b73556d419907216ee89ffb0254d28eda75
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-01-27 07:40:27 +0530

    Add files via upload

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating ArrayList
+            ArrayList My_array = new ArrayList();
+
+            // Adding elements in the 
+            // My_array ArrayList
+            // This ArrayList contains elements
+            // of different types
+            My_array.Add(12.56);
+            My_array.Add("RGVlogs");
+            My_array.Add(null);
+            My_array.Add('R');
+            My_array.Add(1234);
+
+            // Accessing the elements 
+            // of My_array ArrayList
+            // Using foreach loop
+            foreach (var elements in My_array)
+            {
+                Console.WriteLine(elements);
+            }
+            Console.ReadLine();
+        }
+    }
+}
