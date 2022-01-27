e8956b12304afa0ef38951c6270c7942ea54e343
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-01-27 07:40:40 +0530

    Add files via upload

---
+++ ArrayListCountAndCapacity/ArrayListCountAndCapacity/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListCountAndCapacity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an ArrayList 
+            ArrayList myList = new ArrayList();
+
+            // Adding elements to ArrayList 
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+
+            // Displaying count of elements of ArrayList 
+            Console.WriteLine("Number of elements: " + myList.Count);
+
+            // Displaying Current capacity of ArrayList 
+            Console.WriteLine("Current capacity: " + myList.Capacity);
+            Console.ReadLine();
+        }
+    }
+}
