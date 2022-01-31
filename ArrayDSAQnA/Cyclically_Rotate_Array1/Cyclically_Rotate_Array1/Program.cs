397c9715b6df18f776df9abd4d2f8d2c333aeefd
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-01-31 08:05:24 +0530

    Add files via upload

---
+++ ArrayDSAQnA/Cyclically_Rotate_Array1/Cyclically_Rotate_Array1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyclically_Rotate_Array1
+{
+    class Program
+    {
+        static int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+        // Method for rotation
+        static void rotate()
+        {
+            int x = arr[arr.Length - 1], i;
+
+            for (i = arr.Length - 1; i > 0; i--)
+                arr[i] = arr[i - 1];
+            arr[0] = x;
+        }
+
+        // Driver Code
+        public static void Main()
+        {
+            Console.WriteLine("Given Array is");
+            string Original_array = string.Join(" ", arr);
+            Console.WriteLine(Original_array);
+
+            rotate();
+
+            Console.WriteLine("Rotated Array is");
+            string Rotated_array = string.Join(" ", arr);
+            Console.WriteLine(Rotated_array);
+            Console.ReadLine();
+        }
+    }
+}
