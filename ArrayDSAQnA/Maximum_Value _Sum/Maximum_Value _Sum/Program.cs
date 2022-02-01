133f9128cfdffcde7f0e3741dd76fdf288bd9393
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-02-01 08:02:24 +0530

    Add files via upload

---
+++ ArrayDSAQnA/Maximum_Value _Sum/Maximum_Value _Sum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Value__Sum
+{
+    class Program
+    {
+        static int[] arr = new int[]{10, 1, 2, 3, 4,
+                                  5, 6, 7, 8, 9};
+
+        // Returns max possible value of i*arr[i]
+        static int maxSum()
+        {
+            // Find array sum and i*arr[i]
+            // with no rotation
+            int arrSum = 0; // Stores sum of arr[i]
+            int currVal = 0; // Stores sum of i*arr[i]
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrSum = arrSum + arr[i];
+                currVal = currVal + (i * arr[i]);
+            }
+
+            // Initialize result as 0 rotation sum
+            int maxVal = currVal;
+
+            // Try all rotations one by one and find
+            // the maximum rotation sum.
+            for (int j = 1; j < arr.Length; j++)
+            {
+                currVal = currVal + arrSum - arr.Length *
+                                    arr[arr.Length - j];
+                if (currVal > maxVal)
+                    maxVal = currVal;
+            }
+
+            // Return result
+            return maxVal;
+        }
+
+        // Driver Code
+        public static void Main()
+        {
+            Console.WriteLine("Max sum is " + maxSum());
+            Console.ReadLine();
+        }
+    }
+}
