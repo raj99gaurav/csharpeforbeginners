915d8baf58a011c391eb508f0e90d24205216a41
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-02-07 08:14:10 +0530

    Add files via upload

---
+++ DSA450QUESTIONS/Minimum_Jumps_Reach_End_Rec/Minimum_Jumps_Reach_End_Rec/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_Jumps_Reach_End_Rec
+{
+    class Program
+    {
+        static int minJumps(int[] arr, int l, int h)
+        {
+            // Base case: when source
+            // and destination are same
+            if (h == l)
+                return 0;
+
+            // When nothing is reachable
+            // from the given source
+            if (arr[l] == 0)
+                return int.MaxValue;
+
+            // Traverse through all the points
+            // reachable from arr[l]. Recursively
+            // get the minimum number of jumps
+            // needed to reach arr[h] from these
+            // reachable points.
+            int min = int.MaxValue;
+            for (int i = l + 1; i <= h && i <= l + arr[l]; i++)
+            {
+                int jumps = minJumps(arr, i, h);
+                if (jumps != int.MaxValue && jumps + 1 < min)
+                    min = jumps + 1;
+            }
+            return min;
+        }
+
+        // Driver code
+        public static void Main()
+        {
+            int[] arr = { 1, 3, 6, 3, 2, 3, 6, 8, 9, 5 };
+            int n = arr.Length;
+            Console.Write("Minimum number of jumps to reach end is "
+                          + minJumps(arr, 0, n - 1));
+            Console.ReadLine();
+        }
+    }
+}
