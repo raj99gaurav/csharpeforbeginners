eb040f02675a0cb990aa4882989177189ece6f19
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-02-11 08:02:20 +0530

    Add files via upload

---
+++ DSA450QUESTIONS/Stock_Buy_Sell_Maximize_Profit_ValleyPeak/Stock_Buy_Sell_Maximize_Profit_ValleyPeak/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Buy_Sell_Maximize_Profit_ValleyPeak
+{
+    class Program
+    {
+        static int maxProfit(int[] prices, int size)
+        {
+
+            // maxProfit adds up the difference
+            // between adjacent elements if they
+            // are in increasing order
+            int maxProfit = 0;
+
+            // The loop starts from 1 as its
+            // comparing with the previous
+            for (int i = 1; i < size; i++)
+                if (prices[i] > prices[i - 1])
+                    maxProfit += prices[i] - prices[i - 1];
+
+            return maxProfit;
+        }
+
+        // Driver code
+        public static void Main(string[] args)
+        {
+
+            // Stock prices on consecutive days
+            int[] price = { 100, 180, 260, 310, 40, 535, 695 };
+            int n = price.Length;
+
+            // Function call
+            Console.WriteLine(maxProfit(price, n));
+            Console.WriteLine();
+        }
+    }
+}
