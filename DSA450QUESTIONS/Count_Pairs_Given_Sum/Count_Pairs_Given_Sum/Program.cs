a7aaf0e363003992c592d9a3a247c4b30148fdfa
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-02-13 18:34:12 +0530

    Add files via upload

---
+++ DSA450QUESTIONS/Count_Pairs_Given_Sum/Count_Pairs_Given_Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Pairs_Given_Sum
+{
+    class Program
+    {
+        // Returns number of pairs in arr[0..n-1] with sum equal
+        // to 'sum'
+        static int getPairsCount(int[] arr, int n, int k)
+        {
+            Dictionary<int, int> m = new Dictionary<int, int>();
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (m.ContainsKey(k - arr[i]))
+                {
+                    count += m[k - arr[i]];
+                }
+                if (m.ContainsKey(arr[i]))
+                {
+                    m[arr[i]] = m[arr[i]] + 1;
+                }
+                else
+                {
+                    m.Add(arr[i], 1);
+                }
+            }
+            return count;
+        }
+
+        // Driver function to test the above function
+        public static void Main(String[] args)
+        {
+            int[] arr = { 1, 5, 7, -1, 5 };
+            int n = arr.Length;
+            int sum = 6;
+            Console.Write("Count of pairs is " + getPairsCount(arr, n, sum));
+            Console.WriteLine();
+        }
+    }
+}
