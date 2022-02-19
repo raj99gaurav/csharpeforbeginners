573934df90ac577e7c503e59089b4fc5df87d431
Raj Gaurav <49223793+raj99gaurav@users.noreply.github.com>
2022-02-19 22:44:01 +0530

    Add files via upload

---
+++ DSA450QUESTIONS/Duplicate_Array_N+1_Integers/Duplicate_Array_N+1_Integers/Program.cs
@@ -0,0 +1,37 @@
+﻿//find duplicate in an array of N+1 Integers
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duplicate_Array_N_1_Integers
+{
+    class Program
+    {
+        static void printRepeating(int[] arr, int size)
+        {
+            int i, j;
+
+            Console.Write("Repeated Elements are :");
+            for (i = 0; i < size - 1; i++)
+            {
+                for (j = i + 1; j < size; j++)
+                {
+                    if (arr[i] == arr[j])
+                        Console.Write(arr[i] + " ");
+                }
+            }
+        }
+        // driver code
+        public static void Main()
+        {
+            int[] arr = { 4, 2, 4, 5, 2, 3, 1 };
+            int arr_size = arr.Length;
+
+            printRepeating(arr, arr_size);
+
+            Console.WriteLine();
+        }
+    }
+}
